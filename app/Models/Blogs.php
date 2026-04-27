dcd9b98d39fc80ad9c7f79a07fa116eb406210c0
ramadhanzakki <ramadhanzakki506@gmail.com>
2026-04-27 13:16:55 +0700

    feat: move data from route to models

---
+++ app/Models/Blogs.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Blogs {
+
+    public static function all(){
+        return [
+                    [
+                        'id'        => 1,
+                        'blogTitle' => 'Artikel Judul 1',
+                        'author'    => 'Muhammad Zakki Fitra Ramadhan',
+                        'body'      => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe molestias consequatur ipsa ex dicta recusandae at asperiores eveniet dolor blanditiis! Recusandae at id magnam alias dolore praesentium fugiat esse eum.'
+                    ],
+                    [
+                        'id'        => 2,
+                        'blogTitle' => 'Artikel Judul 2',
+                        'author'    => 'Muhammad Zakki Fitra Ramadhan',
+                        'body'      => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eius impedit natus inventore. In sint explicabo quis nemo iusto illo, voluptatem consequatur nisi perferendis quidem ab cum ea debitis consectetur.'
+                    ]
+                ];
+    }
+
+    public static function find($id){
+        return Arr::first(static::all(), function($blog) use ($id){
+            return $blog['id'] == $id;
+        });
+        }
+}
