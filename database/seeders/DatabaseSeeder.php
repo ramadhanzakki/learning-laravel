8f8fcd75e0c9e570d3b09e526bf6cc3d35a4f3c2
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-05-03 10:39:41 +0700

    refactor: split database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,32 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create categories
-        $categoriesFix = ['Web Programming', 'Artificial Inteligence', 'Blockcain and Web3'];
-
-        $categories = Category::factory()
-            ->count(count($categoriesFix))
-            ->sequence(fn ($sequence) => [
-                'name_category' => $categoriesFix[$sequence->index],
-                'slug' => Str::slug($categoriesFix[$sequence->index])
-            ])
-            ->create();
-
-        // Create users
-        $users = User::factory(10)->create();
+        // Call Categories and User
+        $this->call([CategorySeeder::class, UserSeeder::class]);
 
         // Create blogs with existing categories and users
-        Blog::factory(50)->create(function () use ($categories, $users) {
-            return [
-                'category_id' => $categories->random(),
-                'author_id' => $users->random(),
-            ];
-        });
-
-        // Create a test user
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Blog::factory(50)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
